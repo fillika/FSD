ddf7ee71ff105329e7e4d4bece802475a8b902df
fillika <49833052+fillika@users.noreply.github.com>
2019-11-11 23:07:02 +0300

    Остановился на создании функции открывания и закрывания меню

--- src/js/assets/index.js
+++ src/js/assets/index.js
@@ -2,3 +2,23 @@ import $ from "jquery";
 import "popper.js";
 
 console.log("Hello FiLLIka");
+
+const burger = document.querySelector(".burger");
+const closeBtn = document.querySelector(".close-btn");
+
+burger.addEventListener("click", event => {
+  showAsideMenu(event);
+});
+
+closeBtn.addEventListener("click", event => {
+  closeMenu(event);
+});
+
+function showAsideMenu(event) {
+  event.preventDefault();
+
+  const menu = document.querySelector(".header-mobile");
+  menu.classList.add("show");
+}
+
+function closeMenu(event) {}
