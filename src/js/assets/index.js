fe668c9424e101522bcd1125feb50cd393c005a0
fillika <49833052+fillika@users.noreply.github.com>
2019-11-16 22:49:24 +0300

    Добавил класс на открытие и закрытие бургера

--- src/js/assets/index.js
+++ src/js/assets/index.js
@@ -1,24 +1,48 @@
 import $ from "jquery";
 import "popper.js";
 
-console.log("Hello FiLLIka");
+window.addEventListener("load", () => {
+  class Burger {
+    constructor(elem) {
+      this.container = elem;
+      this.menu = document.querySelector(".header-mobile");
 
-const burger = document.querySelector(".burger");
-const closeBtn = document.querySelector(".close-btn");
+      this.init();
+    }
 
-burger.addEventListener("click", event => {
-  showAsideMenu(event);
+    init() {
+      if (!this.container) {
+        console.error("Нет элемента container");
+      } else {
+        this.openMenu();
+        this.closeMenu();
+      }
+    }
+
+    openMenu() {
+      this.container.addEventListener("click", event => {
+        event.preventDefault();
+        this.menu.classList.add("show");
+      });
+    }
+
+    closeMenu() {
+      document.addEventListener("click", e => {
+        for (let i = 0; i < this.menu.childNodes.length; i++) {
+          if (
+            e.target === this.menu ||
+            e.target === this.menu.childNodes[i] ||
+            e.target === this.container
+          ) {
+            return;
+          }
+          this.menu.classList.remove("show");
+        }
+      });
+    }
+  }
+
+  const burgerBtn = document.querySelector(".burger");
+
+  new Burger(burgerBtn);
 });
-
-closeBtn.addEventListener("click", event => {
-  closeMenu(event);
-});
-
-function showAsideMenu(event) {
-  event.preventDefault();
-
-  const menu = document.querySelector(".header-mobile");
-  menu.classList.add("show");
-}
-
-function closeMenu(event) {}
