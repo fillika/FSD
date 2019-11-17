f01a06dcba6e0d0f527b48847b731e353e120c0e
fillika <49833052+fillika@users.noreply.github.com>
2019-11-17 14:18:52 +0300

    Закончил работу над формой. Был создан отдельный класс, который подключается к любому инпуту

--- src/js/assets/index.js
+++ src/js/assets/index.js
@@ -52,6 +52,12 @@ class guestCounter {
     this.input = document.querySelector(selector);
     this.parent = this.input.closest(".guest-container");
     this.container = this.parent.querySelector(".guest-container__inner");
+    this.allOutput = this.container.querySelectorAll(
+      ".guest-container__number"
+    );
+    this.arrow = this.parent.querySelector(".search-form__arrow-down");
+    this.clearBtn = this.parent.querySelector(".guest-container__btn-clear");
+    this.applyBtn = this.parent.querySelector(".guest-container__btn-apply");
 
     this.init();
   }
@@ -59,11 +65,18 @@ class guestCounter {
   init() {
     this.input.addEventListener("click", event => {
       if (this.container.classList.contains("show-guest")) return;
-
+      this.arrow.style.transform = "rotate(180deg)";
+      this.container.classList.add("show-guest");
+    });
+    this.arrow.addEventListener("click", event => {
+      if (this.container.classList.contains("show-guest")) return;
+      this.arrow.style.transform = "rotate(180deg)";
       this.container.classList.add("show-guest");
     });
     this.plus();
     this.minus();
+    this.clear();
+    this.apply();
   }
 
   plus() {
@@ -94,6 +107,39 @@ class guestCounter {
 
         out.innerHTML = value - 1;
       });
+    });
+  }
+
+  clear() {
+    this.clearBtn.addEventListener("click", event => {
+      event.preventDefault();
+      this.allOutput.forEach(item => {
+        item.innerHTML = 0;
+        this.input.value = "";
+      });
+    });
+  }
+
+  apply() {
+    let count = 0;
+
+    this.applyBtn.addEventListener("click", event => {
+      event.preventDefault();
+      this.allOutput.forEach(item => {
+        const val = +item.textContent;
+        count = count + val;
+
+        if (count === 1) {
+          this.input.value = count + " гость";
+        } else if (count >= 2 && count <= 4) {
+          this.input.value = count + " гостя";
+        } else {
+          this.input.value = count + " гостей";
+        }
+      });
+      count = 0;
+      this.container.classList.remove("show-guest");
+      this.arrow.style.transform = "rotate(0deg)";
     });
   }
 }
