6fb31bc4d5dcf8892a4ca4d2962985ad103f6bba
fillika <49833052+fillika@users.noreply.github.com>
2019-11-18 23:45:14 +0300

    Кастомизировал слайдер

--- src/js/assets/plugins.js
+++ src/js/assets/plugins.js
@@ -1,7 +1,18 @@
-$(".js-range-slider").ionRangeSlider({
+const slider = $(".js-range-slider");
+const output = $(".filter__range-total");
+
+slider.ionRangeSlider({
   type: "double",
   min: 0,
-  max: 25000,
+  max: 20000,
   from: 5000,
   to: 10000,
+  hide_min_max: true,
+  hide_from_to: true
+});
+
+slider.on("change", function() {
+  let from = $(this).data("from"); // input data-from attribute
+  let to = $(this).data("to"); // input data-to attribute
+  output.html(`${from}₽ - ${to}₽`);
 });
